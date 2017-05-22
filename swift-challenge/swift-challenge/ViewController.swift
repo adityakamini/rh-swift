3e16603357e7dc8b07641597c46bd0e494d5e617
aditya kamini <aditya.kamini@gm.com>
2017-05-22 14:40:59 -0400

    Initial commit for RH Swift challenge

---
+++ swift-challenge/swift-challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-challenge
+//
+//  Created by aditya kamini on 5/22/17.
+//  Copyright © 2017 aditya kamini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
